447960cd3302dab9d615e029b135ad3957875307
Crasheroo <pawelzal43@gmail.com>
2025-04-15 17:35:46 +0200

    + proxy

--- src/main/java/com/group/Github_Puller/controller/RepoController.java
+++ src/main/java/com/group/Github_Puller/controller/RepoController.java
@@ -1,7 +1,6 @@
 package com.group.Github_Puller.controller;
 
 import com.group.Github_Puller.model.GithubRepoResponse;
-import com.group.Github_Puller.model.Repo;
 import com.group.Github_Puller.service.RepoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +16,5 @@ public class RepoController {
     public ResponseEntity<GithubRepoResponse> getRepo(@PathVariable String owner, @PathVariable String repo) throws Exception {
         GithubRepoResponse response = repoService.getRepo(owner, repo);
         return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/{owner}/{repo}")
-    public ResponseEntity<Repo> createRepo(@PathVariable String owner, @PathVariable String repo) {
-        return null;
-    }
-
-    @DeleteMapping("/{owner}/{repo}")
-    public void deleteRepo(@PathVariable String owner, @PathVariable String repo) {
-        repoService.deleteRepo(owner, repo);
     }
 }
--- src/main/java/com/group/Github_Puller/service/RepoService.java
+++ src/main/java/com/group/Github_Puller/service/RepoService.java
@@ -4,8 +4,6 @@ import com.group.Github_Puller.feign.GithubClient;
 import com.group.Github_Puller.mapper.RepoMapper;
 import com.group.Github_Puller.model.GithubRepoResponse;
 import com.group.Github_Puller.model.Repo;
-import com.group.Github_Puller.repository.RepoRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,19 +14,10 @@ public class RepoService {
     private final GithubClient githubClient;
     private final RepoMapper repoMapper;
 
-    @Transactional
-    public Repo createRepo(String owner, String repoName) throws Exception {
-        return null;
-    }
-
     public GithubRepoResponse getRepo(String owner, String repoName) throws Exception {
         Repo repo = githubClient.getRepo(owner, repoName);
 
         return repoMapper.toDTO(repo);
-    }
-
-    @Transactional
-    public void deleteRepo(String owner, String repoName) {
     }
 }
 
