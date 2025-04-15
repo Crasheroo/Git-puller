ebdc44c360650dc142369ff6a4e848ff9488912e
Crasheroo <pawelzal43@gmail.com>
2025-04-15 17:48:36 +0200

    + class name changes

--- src/main/java/com/group/Github_Puller/controller/RepositoryController.java
+++ src/main/java/com/group/Github_Puller/controller/RepositoryController.java
@@ -3,7 +3,6 @@ package com.group.Github_Puller.controller;
 import com.group.Github_Puller.model.RepositoryDTO;
 import com.group.Github_Puller.service.RepositoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -13,8 +12,7 @@ public class RepositoryController {
     private final RepositoryService repoService;
 
     @GetMapping("/{owner}/{repo}")
-    public ResponseEntity<RepositoryDTO> getRepo(@PathVariable String owner, @PathVariable String repo) throws Exception {
-        RepositoryDTO response = repoService.getRepo(owner, repo);
-        return ResponseEntity.ok(response);
+    public RepositoryDTO getRepo(@PathVariable String owner, @PathVariable String repo) throws Exception {
+        return repoService.getRepo(owner, repo);
     }
 }
