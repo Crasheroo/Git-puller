c9925e61f8c432168f334b0421226b6aea5e99ad
Crasheroo <pawelzal43@gmail.com>
2025-04-15 17:34:59 +0200

    + proxy

--- src/main/java/com/group/Github_Puller/repository/RepoRepository.java
+++ src/main/java/com/group/Github_Puller/repository/RepoRepository.java
@@ -3,9 +3,5 @@ package com.group.Github_Puller.repository;
 import com.group.Github_Puller.model.Repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface RepoRepository extends JpaRepository<Repo, Long> {
-    Optional<Repo> findByFullName(String fullName);
-    boolean existsByFullName(String fullName);
 }
